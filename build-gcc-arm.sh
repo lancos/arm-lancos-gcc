4f5e6ba01803fcac414e3dce3e7575e5e01a2b5a
Claudio Lanconelli <claudiolanconelli@eurek.it>
2013-12-13 12:10:52 +0000

    aggiunto commenti e opzione --with-system-zlib

--- build-gcc-arm.sh
+++ build-gcc-arm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# $Id: build-gcc-arm.sh,v 1.53 2013/09/03 15:48:03 claudio Exp $
+# $Id: build-gcc-arm.sh,v 1.54 2013/11/12 09:03:03 claudio Exp $
 #
 # @brief Build cross compiler for ARM Cortex M3 processor
 # 
 # Builds a bare-metal cross GNU toolchain targetting the ARM Cortex M3
 # microprocessor in EABI mode and using the newlib embedded C library.
 #
-# @version $Revision: 1.53 $
+# @version $Revision: 1.54 $
 # @author  Claudio Lanconelli
 # @note This script was tested on a Ubuntu Linux 8.04 (x86 32/64bit) and
 #       Ubuntu 9.04 but with GCC 4.2.4 (newer version seems to rise some errors)
@@ -412,6 +412,8 @@ cd ${CORTEX_TOPDIR}
 if [ ! -f .gcc ]; then
 	rm -rf gcc-${GCC_VER}
 	tar xfj ${DOWNLOAD_DIR}/gcc-${GCC_VER}.tar.bz2
+#	patch -p0 <gcc_libgcc_divide_exceptions.patch
+#	patch -p0 <gcc_multilib.patch
 	cd gcc-${GCC_VER}
 
 	#cd libstdc++-v3
@@ -429,12 +431,12 @@ if [ ! -f .gcc ]; then
 	mkdir build
 	cd build
 	../configure --target=${TOOLCHAIN_TARGET} --prefix=${TOOLCHAIN_PATH} \
-		--with-cpu=cortex-m3 --with-mode=thumb --enable-interwork --disable-multilib \
+		--with-cpu=cortex-m3 --with-mode=thumb --enable-interwork --with-tune=cortex-m3 --with-float=soft --disable-multilib \
 		--enable-languages="c,c++" --with-newlib --without-headers \
 		--disable-shared --with-gnu-as --with-gnu-ld --with-dwarf2 --enable-initfini-array \
 		--enable-stage1-checking=all --enable-lto --disable-libgomp --disable-libssp --disable-libstdcxx-pch --disable-libmudflap \
-		--disable-nls --with-host-libstdcxx='-lstdc++' \
-		--with-tune=cortex-m3 --with-float=soft --disable-__cxa_atexit \
+		--disable-nls --with-host-libstdcxx='-lstdc++' --with-system-zlib\
+		--disable-__cxa_atexit \
 		--with-gmp=${CORTEX_TOPDIR}/static --with-mpfr=${CORTEX_TOPDIR}/static --with-mpc=${CORTEX_TOPDIR}/static \
 		--with-libelf=${CORTEX_TOPDIR}/static --with-isl=${CORTEX_TOPDIR}/static --with-cloog=${CORTEX_TOPDIR}/static \
 		2>&1 | tee configure.log
@@ -497,6 +499,10 @@ if [ ! -f .newlib ]; then
 
 # Aggiungere per abilitare supporto alle stringhe multi-byte (wide-char)
 #	--enable-newlib-iconv --enable-newlib-mb
+#Linaro:
+#    --enable-newlib-io-long-long --enable-newlib-register-fini --disable-newlib-supplied-syscalls --disable-nls
+#Freddie Chopin:
+#- newlib with different configure options (--enable-newlib-register-fini removed, --enable-newlib-io-c99-formats, --disable-newlib-atexit-dynamic-alloc, --enable-newlib-reent-small, --disable-newlib-fvwrite-in-streamio, --disable-newlib-fseek-optimization, --disable-newlib-wide-orient, --disable-newlib-unbuf-stream-opt) 
 
 	make -j${NUM_JOBS} CFLAGS_FOR_TARGET="-DREENTRANT_SYSCALLS_PROVIDED -DSMALL_MEMORY -DHAVE_ASSERT_FUNC -D__BUFSIZ__=256" 2>&1 | tee make.log
 	make install 2>&1 | tee install.log
